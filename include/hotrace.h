5dabdba22f7e7faca58927d0d367d9d205620527
Matthias Millhoff <mmillhof@student.42berlin.de>
2025-10-18 16:26:14 +0200

    updated header

--- include/hotrace.h
+++ include/hotrace.h
@@ -5,34 +5,38 @@
 
 #include <unistd.h>
 #include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <stdbool.h>
 
 typedef struct s_nodes {
 	char	*key;
 	char	*value;
-	int	index;
+	int		index;
 	struct s_nodes	*next;
 }	t_nodes;
 
 typedef struct s_hashtable {
-	t_nodes **buckets;
-	int	size;
+	t_nodes	**buckets;
+	int		size;
 }	t_hashtable;
 
 
 // UTILS
-size_t ft_strlen(const char *str);
-int	ft_strcmp(char *s1, char *s2);
+size_t	ft_strlen(const char *str);
+int		ft_strcmp(char *s1, char *s2);
 void	ft_strcpy(char *dest, const char *src);
 char	*ft_strdup(const char *src);
-
-
 
 // HASHING 
 t_hashtable	*init_hashes(int size);
 unsigned long long hashing(char	*str);
 void	insert_into_table(t_hashtable *ht, char *key, char *value);
 
-
+// READING
+int	read_next_buffer(char *buf, int fd);
+int	read_next_line(char *line);
+int	read_key_value(char *key, char *value);
 
 
 #endif
